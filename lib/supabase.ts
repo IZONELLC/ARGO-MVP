378456d01104b6796614b93423a748f72ed8f32b
IZONELLC <avaldiviezo@interzone.net>
2025-08-07 10:02:54 -0500

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://wzkcivqayjybpxlkulap.supabase.com!
+const supabaseUrl = process.env.https://wzkcivqayjybpxlkulap.supabase.co!
 const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6a2NpdnFheWp5YnB4bGt1bGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MTIyMzIsImV4cCI6MjA2ODI4ODIzMn0.ycnuQNfwjM3vdLF9gZNE_e5qc8HAHUSTh5f5l515wF8!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
